34487b905e85bde7fd691e4f981f88760b9be10e
berdogan20 <berdogan20@ku.edu.tr>
2023-12-02 12:55:23 +0300

    Initial Commit

---
+++ MassProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MassProject
+//
+//  Created by beyza erdoğan on 2.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MassProject/MassProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MassProjectApp.swift
+//  MassProject
+//
+//  Created by beyza erdoğan on 2.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct MassProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
